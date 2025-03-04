00dc0014a2452193e2d79db63231deb686331b05
Govinrao1 <govindrouthu142@gmail.com>
2025-03-04 22:18:53 +0530

    Added GitHub Actions workflow

--- naukri_update.py
+++ naukri_update.py
@@ -5,9 +5,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 options = Options()
-options.add_argument("--headless")  # Run in headless mode (no GUI)
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--headless")  # Ensures Chrome runs in headless mode (no UI)
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")  # Required in GitHub Actions
+options.add_argument("--disable-dev-shm-usage") 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
