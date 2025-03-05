edce374778cc3b7247ee8be2ba492d8cdcb8daca
Govinrao1 <govindrouthu142@gmail.com>
2025-03-05 14:58:18 +0530

    Added GitHub Actions workflow

--- naukri_update.py
+++ naukri_update.py
@@ -20,8 +20,8 @@ options.add_argument("--window-size=1920,1080")
 options.add_argument("start-maximized")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 # Initialize ChromeDriver
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# driver = webdriver.Chrome(options=options)
 driver.get("https://www.naukri.com/")
 wait = WebDriverWait(driver, 30)  # Increase wait time
 
